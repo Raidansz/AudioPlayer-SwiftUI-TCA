4335c318847d32bc1948f7edac0306ce66cc3049
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-10-18 16:21:08 +0200

    Initial Commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Raidan on 2024. 10. 18..
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
